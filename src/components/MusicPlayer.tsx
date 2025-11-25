372d05bfe535c127b69498fb2e7caea57a33d6c8
Fabricio Garin <fabriciog0911@gmail.com>
2025-11-25 19:15:02 -0300

    feat: update video

--- src/components/MusicPlayer.tsx
+++ src/components/MusicPlayer.tsx
@@ -18,7 +18,7 @@ export default function MusicPlayer({ isPlaying: initialIsPlaying }: MusicPlayer
   // Formato: https://www.youtube.com/embed/VIDEO_ID?autoplay=1&loop=1&playlist=VIDEO_ID&controls=0
   const originParam =
     typeof window !== 'undefined' ? encodeURIComponent(window.location.origin) : ''
-  const musicUrl = `https://www.youtube.com/embed/${YOUTUBE_VIDEO_ID}?autoplay=1&loop=1&playlist=${YOUTUBE_VIDEO_ID}&controls=0&showinfo=0&rel=0&modestbranding=1&enablejsapi=1&mute=1${
+  const musicUrl = `https://www.youtube.com/embed/${YOUTUBE_VIDEO_ID}?autoplay=1&loop=1&playlist=${YOUTUBE_VIDEO_ID}&controls=0&showinfo=0&rel=0&modestbranding=1&enablejsapi=1${
     originParam ? `&origin=${originParam}` : ''
   }`
 
@@ -62,7 +62,7 @@ export default function MusicPlayer({ isPlaying: initialIsPlaying }: MusicPlayer
       {/* Iframe oculto para reproducir música */}
       <iframe
         ref={iframeRef}
-        src={musicUrl}
+        src={isPlaying ? musicUrl : undefined}
         allow="autoplay; encrypted-media; picture-in-picture"
         allowFullScreen
         style={{
